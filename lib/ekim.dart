27279d9a98a79f60776c74bbeff534e94d810f30
hasanaltunbay <altunbayhasan156@gmail.com>
2024-02-05 15:25:07 +0300

    first version.

--- lib/ekim.dart
+++ lib/ekim.dart
@@ -5,9 +5,3 @@ export 'src/context_extension.dart';
 export 'src/elevated_button_extension.dart';
 export 'src/image_extension.dart';
 export 'src/text_extension.dart';
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
